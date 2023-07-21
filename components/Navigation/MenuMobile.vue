f665b7a1aa3c5d2b09eb682bf905fa0bd49842fa
Kazuto <mail@kazuto.de>
2023-07-21 19:47:55 +0200

    fix(nav): mobile menu vertically centered

--- components/Navigation/MenuMobile.vue
+++ components/Navigation/MenuMobile.vue
@@ -2,14 +2,18 @@
   <transition name="overlay">
     <nav
       v-if="isMenuOpen"
-      class="absolute top-0 left-0 w-full h-screen bg-secondary-500 overflow-x-hidden px-10 z-10"
+      class="absolute left-0 top-0 z-10 h-screen w-full overflow-x-hidden bg-secondary-500 px-10"
       :class="{ 'opacity-100': isMenuOpen }"
       role="navigation"
       aria-label="Navigation"
     >
-      <Container class="flex justify-end items-center">
-        <ul class="h-full list-none text-right">
-          <li v-for="(link, index) in items" :key="index" class="my-6">
+      <Container class="flex h-full items-center justify-end">
+        <ul class="list-none text-right">
+          <li
+            v-for="(link, index) in items"
+            :key="index"
+            class="my-6"
+          >
             <NavigationLink
               class="text-3xl"
               :link="link"
@@ -24,7 +28,7 @@
 </template>
 
 <script setup>
-const headerStore = useHeaderStore()
+const headerStore = useHeaderStore();
 
 defineProps({
   items: {
@@ -35,13 +39,13 @@ defineProps({
     type: Number,
     default: 0,
   },
-})
+});
 
-const isMenuOpen = computed(() => headerStore.getMenuState)
+const isMenuOpen = computed(() => headerStore.getMenuState);
 
 const toggleMenu = () => {
-  headerStore.toggleMenu()
-}
+  headerStore.toggleMenu();
+};
 </script>
 
 <style lang="postcss" scoped>
